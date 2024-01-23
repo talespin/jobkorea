1e6c0a60b605c2869b9d8a143fadd5ba18c41637
Choi Jonghwan <newtale@naver.com>
2024-01-23 09:29:10 +0900

    Update jobkorea_crawler.sh

--- src/jobkorea_crawler.sh
+++ src/jobkorea_crawler.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-python jobkorea_crawler.py -l ../list/jobkorea_1.json
-python jobkorea_crawler.py -l ../list/jobkorea_2.json
-python jobkorea_crawler.py -l ../list/jobkorea_3.json
-python jobkorea_crawler.py -l ../list/jobkorea_4.json
-python jobkorea_crawler.py -l ../list/jobkorea_5.json
-python jobkorea_crawler.py -l ../list/jobkorea_6.json
-python jobkorea_crawler.py -l ../list/jobkorea_7.json
-python jobkorea_crawler.py -l ../list/jobkorea_8.json
-python jobkorea_crawler.py -l ../list/jobkorea_9.json
-python jobkorea_crawler.py -l ../list/jobkorea_10.json
+python 3.jobkorea_crawler.py -l ../list/jobkorea_1.json
+python 3.jobkorea_crawler.py -l ../list/jobkorea_2.json
+python 3.jobkorea_crawler.py -l ../list/jobkorea_3.json
+python 3.jobkorea_crawler.py -l ../list/jobkorea_4.json
+python 3.jobkorea_crawler.py -l ../list/jobkorea_5.json
+python 3.jobkorea_crawler.py -l ../list/jobkorea_6.json
+python 3.jobkorea_crawler.py -l ../list/jobkorea_7.json
+python 3.jobkorea_crawler.py -l ../list/jobkorea_8.json
+python 3.jobkorea_crawler.py -l ../list/jobkorea_9.json
+python 3.jobkorea_crawler.py -l ../list/jobkorea_10.json
 
