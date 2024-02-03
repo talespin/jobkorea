76dc2c60031aaa7c4421fb3a1b76f62e889acc24
talespin <newtale@naver.com>
2024-02-03 23:03:58 +0900

    split 시 duplicate 제거

--- src/2.jobkorea_split.py
+++ src/2.jobkorea_split.py
@@ -20,6 +20,7 @@ import sys
 import math
 import logging
 import pandas as pd
+from sklearn.utils import shuffle
 
 
 def main():
@@ -27,11 +28,12 @@ def main():
     if not os.path.exists('../list/jobkorea.xlsx'):
         logging.info(os.path.abspath('../list/jobkorea.xlsx') + ' 파일이 없습니다. jobkorea_list.py 를 실행해서 먼저 리스트파일을 생성하세요')
         return
-    df = pd.read_excel('../list/jobkorea.xlsx').drop_duplicates().reset_index(drop=True)
+    df = shuffle(pd.read_excel('../list/jobkorea.xlsx').drop_duplicates()).reset_index(drop=True)
     if len(df) < split_cnt:
         logging.info('건수보다 나누려는 수가 큽니다.')
         return
     page_cnt = math.ceil(len(df) / split_cnt)
+    fs = [x for x in range(page_cnt)]
     for cnt in range(0, split_cnt):
         df[cnt *page_cnt:(cnt+1)*page_cnt].to_json(f'../list/jobkorea_{cnt+1}.json', orient='records', force_ascii=False)
 
