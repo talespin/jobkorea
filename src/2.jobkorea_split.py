67503756cb648c82aad188d69cbeb8ed1b7968c1
talespin <newtale@naver.com>
2024-02-03 21:20:54 +0900

    split 시 duplicate 제거

--- src/2.jobkorea_split.py
+++ src/2.jobkorea_split.py
@@ -27,7 +27,7 @@ def main():
     if not os.path.exists('../list/jobkorea.xlsx'):
         logging.info(os.path.abspath('../list/jobkorea.xlsx') + ' 파일이 없습니다. jobkorea_list.py 를 실행해서 먼저 리스트파일을 생성하세요')
         return
-    df = pd.read_excel('../list/jobkorea.xlsx')
+    df = pd.read_excel('../list/jobkorea.xlsx').drop_duplicates().reset_index(drop=True)
     if len(df) < split_cnt:
         logging.info('건수보다 나누려는 수가 큽니다.')
         return
