bc8e52f420f42e6d157b044f56ed2a2f2ce8c971
Choi Jonghwan <newtale@naver.com>
2024-02-15 16:42:39 +0900

    Update jobkorea_crawler.sh

--- src/jobkorea_crawler.sh
+++ src/jobkorea_crawler.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-python 3.jobkorea_crawler.py -l ../list/jobkorea_1.json
-python 3.jobkorea_crawler.py -l ../list/jobkorea_2.json
-python 3.jobkorea_crawler.py -l ../list/jobkorea_3.json
-python 3.jobkorea_crawler.py -l ../list/jobkorea_4.json
-python 3.jobkorea_crawler.py -l ../list/jobkorea_5.json
-python 3.jobkorea_crawler.py -l ../list/jobkorea_6.json
-python 3.jobkorea_crawler.py -l ../list/jobkorea_7.json
-python 3.jobkorea_crawler.py -l ../list/jobkorea_8.json
-python 3.jobkorea_crawler.py -l ../list/jobkorea_9.json
-python 3.jobkorea_crawler.py -l ../list/jobkorea_10.json
+cd /mnt/work/jobkorea/src
 
+while :
+do
+	python 1.jobkorea_list.py -y=yes;python 3.jobkorea_crawler_master.py
+        sleep 1h
+done
