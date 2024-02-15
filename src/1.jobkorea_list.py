7282748e2b59f62ce44790bdb223f69dfe86ae70
Choi Jonghwan <newtale@naver.com>
2024-02-15 16:41:13 +0900

    Update 1.jobkorea_list.py

--- src/1.jobkorea_list.py
+++ src/1.jobkorea_list.py
@@ -21,6 +21,7 @@ import sys
 import math
 import urllib3
 import logging
+import argparse
 import pandas as pd
 import requests as req
 from glob import glob
@@ -29,15 +30,16 @@ from bs4 import BeautifulSoup as bs
 
 
 
-def jobkorea_list():
+def jobkorea_list(is_yes):
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     logging.root.name='jobkorea_list'
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     ip = None
     while True:
+        if is_yes: break     
         ip = input('>>>크롤작업을 새로 시작하려면 Y  이어서 하려면 N 를 입력하세요\n')
         if ip in ['Y','N']: break
-    if ip == 'Y': [os.remove(x) for x in glob('../list/*')]
+    if ip == 'Y' or is_yes: [os.remove(x) for x in glob('../list/*')]
     logging.info('start crawl list jobkorea')
     headers = {
         'Accept': 'text/html, */*; q=0.01',
@@ -130,5 +132,8 @@ def jobkorea_list():
 
 
 if __name__=='__main__':
-    jobkorea_list()
+    parser = argparse.ArgumentParser(prog='jobkorea list crawle', description='jobkorea 리스트를 크롤합니다') 
+    parser.add_argument('-y')
+    args = parser.parse_args()
+    jobkorea_list(args.y)
 
