6ede2d10f3175b8d4e15fcc07234a17847c47030
talespin <newtale@naver.com>
2024-02-16 09:39:52 +0900

    crawler shell modify

--- src/jobkorea_crawler.sh
+++ src/jobkorea_crawler.sh
@@ -4,6 +4,8 @@ cd /mnt/work/jobkorea/src
 
 while :
 do
-	python 1.jobkorea_list.py -y=yes;python 3.jobkorea_crawler_master.py
-        sleep 1h
+    python 1.jobkorea_list.py -y=yes
+    python 3.jobkorea_crawler_master.py
+    echo "한시간 쉬었다가 다시 시작합니다"
+    sleep 1h
 done
